ac2cf2bb34919075015d1b03ec72c946ff031dd7
Anna <anna@chaosgoblin.xyz>
2026-06-26 12:11:59 +0200

    Deploy prep: remove AI placeholder posts, add metas plugin + OG tags + homepage welcome text + CSS

--- blog/_config.ts
+++ blog/_config.ts
@@ -1,8 +1,14 @@
 import lume from "lume/mod.ts";
 import blog from "blog/mod.ts";
+import metas from "lume/plugins/metas.ts";
 
-const site = lume();
+const site = lume({
+  location: new URL("https://chaosgoblin.xyz"),
+});
+
 site.use(blog());
+site.use(metas());
+
 site.copy("chaos_inc.png");
 site.copy("favicon.svg");
 
